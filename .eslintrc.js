712eef9d96f4578cd32aec5ca9b88f120f67f136
VInicius Tonashiro <Vinicius.Tonashiro-ext@ab-inbev.com>
2023-01-31 14:21:26 -0300

    feat(core): Create, organize and configure the project

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  root: true,
+  extends: "@react-native-community",
+  parser: "babel-eslint",
+  env: {
+    browser: true,
+  },
+  plugins: ["react-native", "jsx-a11y", "import"],
+  rules: {},
+  settings: {
+    "import/resolver": {
+      node: {
+        paths: ["src"],
+        alias: {
+          _assets: "./src/assets",
+          _components: "./src/components",
+          _routes: "./src/routes",
+          _views: "./src/views",
+          _services: "./src/services",
+          _styles: "./src/styles",
+          _utils: "./src/utils",
+        },
+      },
+    },
+  },
+};
---
+++ src/views/StartGame.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const StartGame = () => {
+  return (
+    <View>
+      <Text>Test</Text>
+    </View>
+  );
+};
+
+export { StartGame };
